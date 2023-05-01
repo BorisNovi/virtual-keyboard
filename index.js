753d86a52c5c64c4e06e6b8feef9e0273612dd06
BorisNovi <112634761+BorisNovi@users.noreply.github.com>
2023-05-02 00:26:25 +0400

    fix: fix forgotten keyboards

--- index.js
+++ index.js
@@ -1,81 +1,84 @@
+"use strict"
+
 function drawHTML() {
-  // casedown, caseup, caps, shiftcaps. Cледующие 4 - то же самое, но на русском
-  const numObj = {
-    Backquote: ['&#x60;', '~', '&#x60;', '~', 'ё', 'Ё', 'Ё', 'ё'],
-    Digit1: ['1', '!', '1', '!', '1', '!', '1', '!'],
-    Digit2: ['2', '@', '2', '@', '2', '"', '2', '"'],
-    Digit3: ['3', '#', '3', '#', '3', '№', '3', '№'],
-    Digit4: ['4', '$', '4', '$', '4', ';', '4', ';'],
-    Digit5: ['5', '%', '5', '%', '5', '%', '5', '%'],
-    Digit6: ['6', '^', '6', '^', '6', ':', '6', ':'],
-    Digit7: ['7', '&', '7', '&', '7', '?', '7', '?'],
-    Digit8: ['8', '*', '8', '*', '8', '*', '8', '*'],
-    Digit9: ['9', '&lpar;', '9', '&lpar;', '9', '&lpar;', '9', '&lpar;'],
-    Digit0: ['0', '&rpar;', '0', '&rpar;', '0', '&rpar;', '0', '&rpar;'],
-    Minus: ['-', '_', '-', '_', '-', '_', '-', '_'],
-    Equal: ['=', '+', '=', '+', '=', '+', '=', '+'],
-    Backspace: ['Back', 'Back', 'Back', 'Back', '&larr;', '&larr;', '&larr;', '&larr;'],
-    }
-  const tabObj = {
-    Tab: ['Tab', 'Tab', 'Tab', 'Tab', 'Таб', 'Таб', 'Таб', 'Таб'],
-    KeyQ: ['q', 'Q', 'Q', 'q', 'й', 'Й', 'Й', 'й'],
-    KeyW: ['w', 'W', 'W', 'w', 'ц', 'Ц', 'Ц', 'ц'],
-    KeyE: ['e', 'E', 'E', 'e', 'у', 'У', 'У', 'у'],
-    KeyR: ['r', 'R', 'R', 'r', 'к', 'К', 'К', 'к'],
-    KeyT: ['t', 'T', 'T', 't', 'е', 'Е', 'Е', 'е'],
-    KeyY: ['y', 'Y', 'Y', 'y', 'н', 'Н', 'Н', 'н'],
-    KeyU: ['u', 'U', 'U', 'u', 'г', 'Г', 'Г', 'г'],
-    KeyI: ['i', 'I', 'I', 'i', 'ш', 'Ш', 'Ш', 'ш'],
-    KeyO: ['o', 'O', 'O', 'o', 'щ', 'Щ', 'Щ', 'щ'],
-    KeyP: ['p', 'P', 'P', 'p', 'з', 'З', 'З', 'з'],
-    BracketLeft: ['&lbrack;', '&lcub;', '&lbrack;', '&lcub;', 'х', 'Х', 'Х', 'х'],
-    BracketRight: ['&rbrack;', '&rcub;', '&rbrack;', '&rcub;', 'ъ', 'Ъ', 'Ъ', 'ъ'],
-    Backslash: ['\\', '|', '\\', '|', '\\', '/', '\\', '/'],
-    Delete: ['Del', 'Del', 'Del', 'Del', 'Удал', 'Удал', 'Удал', 'Удал']
-  };
-  const capsObj = {
-    CapsKey: ['Caps', 'Caps', 'Caps', 'Caps', 'Кричать', 'Кричать', 'Кричать', 'Кричать'],
-    KeyA: ['a', 'A', 'A', 'a', 'ф', 'Ф', 'Ф', 'ф'],
-    KeyS: ['s', 'S', 'S', 's', 'ы', 'Ы', 'Ы', 'ы'],
-    KeyD: ['d', 'D', 'D', 'd', 'в', 'В', 'В', 'в'],
-    KeyF: ['f', 'F', 'F', 'f', 'а', 'А', 'А', 'а'],
-    KeyG: ['g', 'G', 'G', 'g', 'п', 'П', 'П', 'п'],
-    KeyH: ['h', 'H', 'H', 'h', 'р', 'Р', 'Р', 'р'],
-    KeyJ: ['j', 'J', 'J', 'j', 'о', 'О', 'О', 'о'],
-    KeyK: ['k', 'K', 'K', 'k', 'л', 'Л', 'Л', 'л'],
-    KeyL: ['l', 'L', 'L', 'l', 'д', 'Д', 'Д', 'д'],
-    Semicolon: [';', ':', ';', ';', 'ж', 'Ж', 'Ж', 'ж'],
-    Quote: ["'", '"', "'", '"', 'э', 'Э', 'Э', 'э'],
-    Enter: ['Enter', 'Enter', 'Enter', 'Enter', 'Ввод', 'Ввод', 'Ввод', 'Ввод']
-  };
-  const shiftObj = {
-    shiftKey: ['Shift', 'Shift', 'Shift', 'Shift', 'Большие', 'Большие', 'Большие', 'Большие'],
-    KeyZ: ['z', 'Z', 'Z', 'z', 'я', 'Я', 'Я', 'я'],
-    KeyX: ['x', 'X', 'X', 'x', 'ч', 'Ч', 'Ч', 'ч'],
-    KeyC: ['c', 'C', 'C', 'c', 'с', 'С', 'С', 'с'],
-    KeyV: ['v', 'V', 'V', 'v', 'м', 'М', 'М', 'м'],
-    KeyB: ['b', 'B', 'B', 'b', 'и', 'И', 'И', 'и'],
-    KeyN: ['n', 'N', 'N', 'n', 'т', 'Т', 'Т', 'т'],
-    KeyM: ['m', 'M', 'M', 'm', 'ь', 'Ь', 'Ь', 'ь'],
-    Comma: [',', '&lt', ',', '&lt', 'б', 'Б', 'Б', 'б'],
-    Period: ['.', '&gt', '.', '&gt', 'ю', 'Ю', 'Ю', 'ю'],
-    Slash: ['/', '?', '/', '?', '.', ',', '.', ','],
-    ArrowUp: ['&#9650;', '&#9650;', '&#9650;', '&#9650;', '&#9650;', '&#9650;', '&#9650;', '&#9650;'],
-    rshiftKey: ['Shift', 'Shift', 'Shift', 'Shift', 'Большие', 'Большие', 'Большие', 'Большие']
-  };
-  const ctrlObj = {
-    ControlLeft: ['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl', 'Ктрл', 'Ктрл', 'Ктрл', 'Ктрл'],
-    MetaLeft: ['Win', 'Win', 'Win', 'Win', 'Окна', 'Окна', 'Окна', 'Окна'],
-    AltLeft: ['Alt', 'Alt', 'Alt', 'Alt', 'Альт', 'Альт', 'Альт', 'Альт'],
-    Space: ['Space', 'Space', 'Space', 'Space', 'Пробел', 'Пробел', 'Пробел', 'Пробел'],
-    AltRight: ['Alt', 'Alt', 'Alt', 'Alt', 'Альт', 'Альт', 'Альт', 'Альт'],
-    ControlRight: ['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl', 'Ктрл', 'Ктрл', 'Ктрл', 'Ктрл'],
-    ArrowLeft: ['&#9668;', '&#9668;', '&#9668;', '&#9668;', '&#9668;', '&#9668;', '&#9668;', '&#9668;',],
-    ArrowDown: ['&#9660;', '&#9660;', '&#9660;', '&#9660;', '&#9660;', '&#9660;', '&#9660;', '&#9660;'],
-    ArrowRight: ['&#9658;', '&#9658;', '&#9658;', '&#9658;', '&#9658;', '&#9658;', '&#9658;', '&#9658;'],
-    fnKey: ['EN', 'EN', 'EN', 'EN', 'RU', 'RU', 'RU', 'RU']
-  };
-  document.body.innerHTML = `<div class="container">
+    // casedown, caseup, caps, shiftcaps. Cледующие 4 - то же самое, но на русском
+    const numObj = {
+        Backquote: ['&#x60;', '~', '&#x60;', '~', 'ё', 'Ё', 'Ё', 'ё'],
+        Digit1: ['1', '!', '1', '!', '1', '!', '1', '!'],
+        Digit2: ['2', '@', '2', '@', '2', '"', '2', '"'],
+        Digit3: ['3', '#', '3', '#', '3', '№', '3', '№'],
+        Digit4: ['4', '$', '4', '$', '4', ';', '4', ';'],
+        Digit5: ['5', '%', '5', '%', '5', '%', '5', '%'],
+        Digit6: ['6', '^', '6', '^', '6', ':', '6', ':'],
+        Digit7: ['7', '&', '7', '&', '7', '?', '7', '?'],
+        Digit8: ['8', '*', '8', '*', '8', '*', '8', '*'],
+        Digit9: ['9', '&lpar;', '9', '&lpar;', '9', '&lpar;', '9', '&lpar;'],
+        Digit0: ['0', '&rpar;', '0', '&rpar;', '0', '&rpar;', '0', '&rpar;'],
+        Minus: ['-', '_', '-', '_', '-', '_', '-', '_'],
+        Equal: ['=', '+', '=', '+', '=', '+', '=', '+'],
+        Backspace: ['Back', 'Back', 'Back', 'Back', '&larr;', '&larr;', '&larr;', '&larr;'],
+    };
+    const tabObj = {
+        Tab: ['Tab', 'Tab', 'Tab', 'Tab', 'Таб', 'Таб', 'Таб', 'Таб'],
+        KeyQ: ['q', 'Q', 'Q', 'q', 'й', 'Й', 'Й', 'й'],
+        KeyW: ['w', 'W', 'W', 'w', 'ц', 'Ц', 'Ц', 'ц'],
+        KeyE: ['e', 'E', 'E', 'e', 'у', 'У', 'У', 'у'],
+        KeyR: ['r', 'R', 'R', 'r', 'к', 'К', 'К', 'к'],
+        KeyT: ['t', 'T', 'T', 't', 'е', 'Е', 'Е', 'е'],
+        KeyY: ['y', 'Y', 'Y', 'y', 'н', 'Н', 'Н', 'н'],
+        KeyU: ['u', 'U', 'U', 'u', 'г', 'Г', 'Г', 'г'],
+        KeyI: ['i', 'I', 'I', 'i', 'ш', 'Ш', 'Ш', 'ш'],
+        KeyO: ['o', 'O', 'O', 'o', 'щ', 'Щ', 'Щ', 'щ'],
+        KeyP: ['p', 'P', 'P', 'p', 'з', 'З', 'З', 'з'],
+        BracketLeft: ['&lbrack;', '&lcub;', '&lbrack;', '&lcub;', 'х', 'Х', 'Х', 'х'],
+        BracketRight: ['&rbrack;', '&rcub;', '&rbrack;', '&rcub;', 'ъ', 'Ъ', 'Ъ', 'ъ'],
+        Backslash: ['\\', '|', '\\', '|', '\\', '/', '\\', '/'],
+        Delete: ['Del', 'Del', 'Del', 'Del', 'Удал', 'Удал', 'Удал', 'Удал']
+    };
+    const capsObj = {
+        CapsKey: ['Caps', 'Caps', 'Caps', 'Caps', 'Кричать', 'Кричать', 'Кричать', 'Кричать'],
+        KeyA: ['a', 'A', 'A', 'a', 'ф', 'Ф', 'Ф', 'ф'],
+        KeyS: ['s', 'S', 'S', 's', 'ы', 'Ы', 'Ы', 'ы'],
+        KeyD: ['d', 'D', 'D', 'd', 'в', 'В', 'В', 'в'],
+        KeyF: ['f', 'F', 'F', 'f', 'а', 'А', 'А', 'а'],
+        KeyG: ['g', 'G', 'G', 'g', 'п', 'П', 'П', 'п'],
+        KeyH: ['h', 'H', 'H', 'h', 'р', 'Р', 'Р', 'р'],
+        KeyJ: ['j', 'J', 'J', 'j', 'о', 'О', 'О', 'о'],
+        KeyK: ['k', 'K', 'K', 'k', 'л', 'Л', 'Л', 'л'],
+        KeyL: ['l', 'L', 'L', 'l', 'д', 'Д', 'Д', 'д'],
+        Semicolon: [';', ':', ';', ';', 'ж', 'Ж', 'Ж', 'ж'],
+        Quote: ["'", '"', "'", '"', 'э', 'Э', 'Э', 'э'],
+        Enter: ['Enter', 'Enter', 'Enter', 'Enter', 'Ввод', 'Ввод', 'Ввод', 'Ввод']
+    };
+    const shiftObj = {
+        shiftKey: ['Shift', 'Shift', 'Shift', 'Shift', 'Большие', 'Большие', 'Большие', 'Большие'],
+        KeyZ: ['z', 'Z', 'Z', 'z', 'я', 'Я', 'Я', 'я'],
+        KeyX: ['x', 'X', 'X', 'x', 'ч', 'Ч', 'Ч', 'ч'],
+        KeyC: ['c', 'C', 'C', 'c', 'с', 'С', 'С', 'с'],
+        KeyV: ['v', 'V', 'V', 'v', 'м', 'М', 'М', 'м'],
+        KeyB: ['b', 'B', 'B', 'b', 'и', 'И', 'И', 'и'],
+        KeyN: ['n', 'N', 'N', 'n', 'т', 'Т', 'Т', 'т'],
+        KeyM: ['m', 'M', 'M', 'm', 'ь', 'Ь', 'Ь', 'ь'],
+        Comma: [',', '&lt', ',', '&lt', 'б', 'Б', 'Б', 'б'],
+        Period: ['.', '&gt', '.', '&gt', 'ю', 'Ю', 'Ю', 'ю'],
+        Slash: ['/', '?', '/', '?', '.', ',', '.', ','],
+        ArrowUp: ['&#9650;', '&#9650;', '&#9650;', '&#9650;', '&#9650;', '&#9650;', '&#9650;', '&#9650;'],
+        rshiftKey: ['Shift', 'Shift', 'Shift', 'Shift', 'Большие', 'Большие', 'Большие', 'Большие']
+    };
+    const ctrlObj = {
+        ControlLeft: ['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl', 'Ктрл', 'Ктрл', 'Ктрл', 'Ктрл'],
+        MetaLeft: ['Win', 'Win', 'Win', 'Win', 'Окна', 'Окна', 'Окна', 'Окна'],
+        AltLeft: ['Alt', 'Alt', 'Alt', 'Alt', 'Альт', 'Альт', 'Альт', 'Альт'],
+        Space: ['Space', 'Space', 'Space', 'Space', 'Пробел', 'Пробел', 'Пробел', 'Пробел'],
+        AltRight: ['Alt', 'Alt', 'Alt', 'Alt', 'Альт', 'Альт', 'Альт', 'Альт'],
+        ControlRight: ['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl', 'Ктрл', 'Ктрл', 'Ктрл', 'Ктрл'],
+        ArrowLeft: ['&#9668;', '&#9668;', '&#9668;', '&#9668;', '&#9668;', '&#9668;', '&#9668;', '&#9668;',],
+        ArrowDown: ['&#9660;', '&#9660;', '&#9660;', '&#9660;', '&#9660;', '&#9660;', '&#9660;', '&#9660;'],
+        ArrowRight: ['&#9658;', '&#9658;', '&#9658;', '&#9658;', '&#9658;', '&#9658;', '&#9658;', '&#9658;'],
+        fnKey: ['EN', 'EN', 'EN', 'EN', 'RU', 'RU', 'RU', 'RU']
+    };
+
+    document.body.innerHTML = `<div class="container">
     <h1 class="header">RS Virtual Keyboard</h1>
     <textarea class="keyboard-textarea" id="textarea" cols="50" rows="8"></textarea>
     <div class="keyboard-keys">
@@ -88,14 +91,15 @@ function drawHTML() {
     <p class="keyboard-notice">Переключение языка - ShiftLeft + AltLeft, либо кнопка с указанием языка.</p>
     <p class="keyboard-notice">Создано в ОС Windows.</p>
 `; // Body
-  const NUM_INSERT = document.querySelector('.numkeys');
-  const TAB_INSERT = document.querySelector('.tabkeys');
-  const CAPS_INSERT = document.querySelector('.capskeys');
-  const SHIFT_INSERT = document.querySelector('.shiftkeys');
-  const CTRL_INSERT = document.querySelector('.ctrlkeys');
 
-  for (let i = 0; i < Object.keys(numObj).length; i++) {
-    NUM_INSERT.innerHTML += `<div class="keyboard-key ${Object.keys(numObj)[i]}">
+    const NUM_INSERT = document.querySelector('.numkeys');
+    const TAB_INSERT = document.querySelector('.tabkeys');
+    const CAPS_INSERT = document.querySelector('.capskeys');
+    const SHIFT_INSERT = document.querySelector('.shiftkeys');
+    const CTRL_INSERT = document.querySelector('.ctrlkeys');
+
+    for (let i = 0; i < Object.keys(numObj).length; i++) {
+        NUM_INSERT.innerHTML += `<div class="keyboard-key ${Object.keys(numObj)[i]}">
 <span class="rus hidden">
     <span class="caseDown hidden">${numObj[Object.keys(numObj)[i]][4]}</span>
     <span class="caseUp hidden">${numObj[Object.keys(numObj)[i]][5]}</span>
@@ -109,9 +113,10 @@ function drawHTML() {
     <span class="shiftCaps hidden">${numObj[Object.keys(numObj)[i]][3]}</span>
 </span>
 </div>`
-  } // Num row
-  for (let i = 0; i < Object.keys(tabObj).length; i++) {
-    TAB_INSERT.innerHTML += `<div class="keyboard-key ${Object.keys(tabObj)[i]}">
+    } // Num row
+
+    for (let i = 0; i < Object.keys(tabObj).length; i++) {
+        TAB_INSERT.innerHTML += `<div class="keyboard-key ${Object.keys(tabObj)[i]}">
 <span class="rus hidden">
     <span class="caseDown hidden">${tabObj[Object.keys(tabObj)[i]][4]}</span>
     <span class="caseUp hidden">${tabObj[Object.keys(tabObj)[i]][5]}</span>
@@ -125,9 +130,10 @@ function drawHTML() {
     <span class="shiftCaps hidden">${tabObj[Object.keys(tabObj)[i]][3]}</span>
 </span>
 </div>`
-  } // Tab row
-  for (let i = 0; i < Object.keys(capsObj).length; i++) {
-    CAPS_INSERT.innerHTML += `<div class="keyboard-key ${Object.keys(capsObj)[i]}">
+    } // Tab row
+
+    for (let i = 0; i < Object.keys(capsObj).length; i++) {
+        CAPS_INSERT.innerHTML += `<div class="keyboard-key ${Object.keys(capsObj)[i]}">
 <span class="rus hidden">
     <span class="caseDown hidden">${capsObj[Object.keys(capsObj)[i]][4]}</span>
     <span class="caseUp hidden">${capsObj[Object.keys(capsObj)[i]][5]}</span>
@@ -141,10 +147,10 @@ function drawHTML() {
     <span class="shiftCaps hidden">${capsObj[Object.keys(capsObj)[i]][3]}</span>
 </span>
 </div>`
-  } // Caps row
+    } // Caps row
 
-  for (let i = 0; i < Object.keys(shiftObj).length; i++) {
-    SHIFT_INSERT.innerHTML += `<div class="keyboard-key ${Object.keys(shiftObj)[i]}">
+    for (let i = 0; i < Object.keys(shiftObj).length; i++) {
+        SHIFT_INSERT.innerHTML += `<div class="keyboard-key ${Object.keys(shiftObj)[i]}">
 <span class="rus hidden">
     <span class="caseDown hidden">${shiftObj[Object.keys(shiftObj)[i]][4]}</span>
     <span class="caseUp hidden">${shiftObj[Object.keys(shiftObj)[i]][5]}</span>
@@ -158,10 +164,10 @@ function drawHTML() {
     <span class="shiftCaps hidden">${shiftObj[Object.keys(shiftObj)[i]][3]}</span>
 </span>
 </div>`
-  } // Shift row
+    } // Shift row
 
-  for (let i = 0; i < Object.keys(ctrlObj).length; i++) {
-    CTRL_INSERT.innerHTML += `<div class="keyboard-key ${Object.keys(ctrlObj)[i]}">
+    for (let i = 0; i < Object.keys(ctrlObj).length; i++) {
+        CTRL_INSERT.innerHTML += `<div class="keyboard-key ${Object.keys(ctrlObj)[i]}">
 <span class="rus hidden">
     <span class="caseDown hidden">${ctrlObj[Object.keys(ctrlObj)[i]][4]}</span>
     <span class="caseUp hidden">${ctrlObj[Object.keys(ctrlObj)[i]][5]}</span>
@@ -175,9 +181,10 @@ function drawHTML() {
     <span class="shiftCaps hidden">${ctrlObj[Object.keys(ctrlObj)[i]][3]}</span>
 </span>
 </div>`
-  } // Ctrl row
+    } // Ctrl row
 }
 drawHTML();
+
 const KEYBOARD = document.querySelectorAll('.keyboard-key');
 const CASE_DOWN = document.querySelectorAll('.caseDown');
 const CASE_UP = document.querySelectorAll('.caseUp');
@@ -252,8 +259,7 @@ function caseToggle(caseName) {
             element.classList.remove('hidden');
         });
     }
-} // передаем название кейса - снимается его хидден, с этого кейса.
-//Добавляется хидден к остальным кейсам.
+} // передаем название кейса - снимается его хидден, с этого кейса. Добавляется хидден к остальным кейсам.
 
 let isCapsLocked = false;
 function turnOnCaps() {
@@ -422,14 +428,18 @@ function typeKeyboard() {
         document.addEventListener('keydown', event => {
             event.preventDefault();
             TEXTAREA.focus();
-
             if (event.code === key.classList[1]) {
                 if (event.code.slice(0, 3) === 'Key' ||
                     event.code.slice(0, 3) === 'Dig' ||
                     event.code.slice(0, 3) === 'Min' ||
                     event.code.slice(0, 3) === 'Equ' ||
                     event.code.slice(0, 3) === 'Bra' ||
+                    event.code == 'Backquote' ||
                     event.code == 'Backslash' ||
+                    event.code == 'Semicolon' ||
+                    event.code == 'Quote' ||
+                    event.code == 'Comma' ||
+                    event.code == 'Period' ||
                     event.code == 'Slash') {
 
                     TEXTAREA.value += key.innerText;
@@ -470,11 +480,6 @@ function typeKeyboard() {
                 }
             }
         });
-        document.addEventListener('keyup', event => {
-            if (event.code != 'CapsLock') {
-                // key.classList.remove("pushed");
-            }
-        });
     });
 }
 typeKeyboard()
@@ -484,13 +489,17 @@ function typeMouse() {
         KEYBOARD[i].addEventListener('mousedown', () => {
             let key = KEYBOARD[i];
             let keyCode = KEYBOARD[i].classList[1];
-
             if (keyCode.slice(0, 3) === 'Key' ||
                 keyCode.slice(0, 3) === 'Dig' ||
                 keyCode.slice(0, 3) === 'Min' ||
                 keyCode.slice(0, 3) === 'Equ' ||
                 keyCode.slice(0, 3) === 'Bra' ||
+                keyCode == 'Backquote' ||
                 keyCode == 'Backslash' ||
+                keyCode == 'Semicolon' ||
+                keyCode == 'Quote' ||
+                keyCode == 'Comma' ||
+                keyCode == 'Period' ||
                 keyCode == 'Slash') {
                 TEXTAREA.value += key.innerText;
             }
@@ -606,6 +615,3 @@ function moveCursorByClick() {
     }
 }
 moveCursorByClick();
-
-var fuck = 0;
-console.log(fuck);
